9d8438cc8bb9bf07de9fb60bc6776808849fa72d
santiarco92 <santiago_arco@hotmail.com>
2024-11-03 14:01:05 -0500

    Inicio de Que Hacemos

--- src/components/Quehacemos.jsx
+++ src/components/Quehacemos.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import Grid from '@mui/material/Grid2';
+import { Grid } from "@mui/material";
 
-import Pagina6 from "../img/PAGINA 6.jpg";
-import Pagina7 from "../img/PAGINA 7.jpg";
-import Pagina8 from "../img/PAGINA 8.jpg";
-
-
-
-function Quehacemos () {
-    return (
-       <h1>jhola</h1>
-    )
+function Quehacemos() {
+  return (
+    <Grid container className="que-hacemos-container">
+      <Grid item>
+        <h1 className="titles">Que hacemos</h1>
+      </Grid>
+      <Grid>
+        
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Quehacemos;
